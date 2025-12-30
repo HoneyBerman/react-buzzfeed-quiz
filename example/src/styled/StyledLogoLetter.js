108f04facb15541084c60d4f2ab151a6e3086811
Honey Berman <46846115+HoneyBerman@users.noreply.github.com>
2025-12-31 00:24:08 +0530

    latest changes

--- example/src/styled/StyledLogoLetter.js
+++ example/src/styled/StyledLogoLetter.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import { __makeTemplateObject } from "tslib";
 import styled from "styled-components";
 var StyledLogoLetter = styled.h1(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  color: ", ";\n\n  font-size: 1.5rem;\n  padding-right: 0.4rem;\n\n  @media (max-width: 375px) {\n    font-size: 1rem;\n    padding-right: 0.3rem;\n  }\n\n  @media (min-width: 1200px) {\n    font-size: 2rem;\n    padding-right: 0.5rem;\n  }\n"], ["\n  color: ", ";\n\n  font-size: 1.5rem;\n  padding-right: 0.4rem;\n\n  @media (max-width: 375px) {\n    font-size: 1rem;\n    padding-right: 0.3rem;\n  }\n\n  @media (min-width: 1200px) {\n    font-size: 2rem;\n    padding-right: 0.5rem;\n  }\n"])), function (props) { return (props.buzzfeed ? "rgb(238, 50, 34)" : "#000"); });
